34a5ba108041fd60d044beae06e60f14b9d2c773
A-Gray-94 <A.ChristisonGray@student.tudelft.nl>
2019-07-20 18:24:05 -0400

    Reorganised file structure

---
+++ bin/plotPoints.m
@@ -0,0 +1,6 @@
+function plotPoints(inp,varargin)
+    plotfcn = p@(x)drawSphere(x,10, 'facecolor', 'r');
+    
+    % Figure out if user has input a corner structure, a front or rear
+    % structure or the whole geom structure
+end
--- plotPoints.m
+++
@@ -1,6 +0,0 @@
-function plotPoints(inp,varargin)
-    plotfcn = p@(x)drawSphere(x,10, 'facecolor', 'r');
-    
-    % Figure out if user has input a corner structure, a front or rear
-    % structure or the whole geom structure
-end
